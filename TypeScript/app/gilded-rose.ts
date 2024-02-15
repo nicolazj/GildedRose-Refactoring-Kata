f735544d6ea93a581c5f8bd8cf5e13295021752c
Nicolas Jiang <nicolazj@gmail.com>
2024-02-15 21:23:33 +1300

    refactoring:  more methods and tests

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -24,58 +24,81 @@ export class GildedRose {
     this.items = items;
   }
 
+  updateSulfuras(item: Item) {
+    // “Sulfuras”, being a legendary item, never has to be sold or decreases in Quality
+    // so does nothing
+  }
+  updateAgedBrie(item: Item) {
+    // “Aged Brie” actually increases in Quality the older it gets
+    if (item.sellIn > 0) {
+      item.quality += 1;
+    } else {
+      // Once the sell by date has passed, Quality degrades twice as fast
+      item.quality += 2;
+    }
+  }
+  updateBackstagePasses(item: Item) {
+    if (item.sellIn > 10) {
+      item.quality += 1;
+    } else if (item.sellIn > 5) {
+      // Quality increases by 2 when there are 10 days or less
+      item.quality += 2;
+    } else if (item.sellIn > 0) {
+      //and by 3 when there are 5 days or less but
+      item.quality += 3;
+    } else {
+      // Quality drops to 0 after the concert
+      item.quality = 0;
+    }
+  }
+  updateConjuredItem(item: Item) {
+    //“Conjured” items degrade in Quality twice as fast as normal items
+    if (item.sellIn > 0) {
+      item.quality -= 2;
+    } else {
+      // Once the sell by date has passed, Quality degrades twice as fast
+      item.quality -= 4;
+    }
+  }
+
+  updateNormalItem(item: Item) {
+    // Once the sell by date has passed, Quality degrades twice as fast
+    if (item.sellIn > 0) {
+      item.quality -= 1;
+    } else item.quality -= 2;
+  }
+
+  updateSellIn(item: Item) {
+    item.sellIn += -1;
+  }
+  clampQuality(item: Item) {
+    if (item.quality > MAX_QUALITY) item.quality = MAX_QUALITY;
+    if (item.quality < MIN_QUALITY) item.quality = MIN_QUALITY;
+  }
+
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       let item = this.items[i];
+      // "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+      // early return;
+      if (item.name === ITEM_SULFURAS) continue;
 
       switch (item.name) {
-        case ITEM_SULFURAS:
-          // “Sulfuras”, being a legendary item, never has to be sold or decreases in Quality
-          break;
         case ITEM_AGED_BRIE:
-          // “Aged Brie” actually increases in Quality the older it gets
-          item.quality += 1;
-          // Once the sell by date has passed, Quality degrades twice as fast
-          if (item.sellIn <= 0) {
-            item.quality += 1;
-          }
-          item.quality = Math.min(item.quality, MAX_QUALITY);
-          item.sellIn += -1;
+          this.updateAgedBrie(item);
           break;
-
         case ITEM_BACKSTAGE_PASSES:
-          // Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
-          // Quality drops to 0 after the concert
-          if (item.sellIn > 10) {
-            item.quality += 1;
-          } else if (item.sellIn > 5) {
-            item.quality += 2;
-          } else if (item.sellIn > 0) {
-            item.quality += 3;
-          } else {
-            item.quality = 0;
-          }
-          item.quality = Math.min(item.quality, MAX_QUALITY);
-          item.sellIn += -1;
+          this.updateBackstagePasses(item);
           break;
-
         case ITEM_CONJURED:
-          //“Conjured” items degrade in Quality twice as fast as normal items
-          item.quality -= 2;
-          // Once the sell by date has passed, Quality degrades twice as fast
-          if (item.sellIn <=0) item.quality -= 2;
-          item.quality = Math.max(item.quality, MIN_QUALITY);
-          item.sellIn += -1;
+          this.updateConjuredItem(item);
           break;
-
         default:
-          // Once the sell by date has passed, Quality degrades twice as fast
-          if (item.sellIn <= 0) item.quality -= 1 * 2;
-          else item.quality -= 1;
-          item.quality = Math.max(item.quality, MIN_QUALITY);
-          item.sellIn += -1;
+          this.updateNormalItem(item);
           break;
       }
+      this.clampQuality(item);
+      this.updateSellIn(item);
     }
 
     return this.items;
